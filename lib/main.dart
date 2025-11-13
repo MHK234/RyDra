5c1f46efef9f7ae0ac6b6fff47b31f851790f6b3
MHK234 <hk7265234@gmail.com>
2025-11-14 02:09:18 +0500

    Added Splash Screen and HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import './screens/SplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,26 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'My App Home'),
+      title: 'Rydra',
+      home: const SplashScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello, Welcome to Rydra')));
   }
 }
